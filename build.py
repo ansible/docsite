4ae53f572df1ea141a0c52dc5fc250f8409a261f
Don Naro <dnaro@redhat.com>
2024-02-28 13:02:08 +0000

    set base url via build.py
    Co-authored-by: Sviatoslav Sydorenko <578543+webknjaz@users.noreply.github.com>

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
 import shutil
 import sys
+from functools import partial
 from pathlib import Path
 
 from staticjinja import Site
@@ -8,11 +9,15 @@ from yaml import Loader, load
 import sass
 
 
-def load_page_data():
-    yaml = Path("./data").glob("*.yaml")
-    return {
-        file_path.stem: load(file_path.read_text(), Loader=Loader) for file_path in yaml
+def load_page_data(template, /, *, base_url=None):
+    context_objects = Path("./data").glob("*.yaml")
+    context_mapping = {
+        file_path.stem: load(file_path.read_text(), Loader=Loader)
+        for file_path in context_objects
     }
+    if base_url is not None:
+        context_mapping["base"]["base_url"] = base_url
+    return context_mapping
 
 
 def clean_buildpath(output_dir):
@@ -25,12 +30,19 @@ def main():
     except IndexError:
         build_dir_str = "output"
 
+    try:
+        custom_domain_cli_arg = sys.argv[2]
+    except IndexError:
+        custom_domain_cli_arg = None
+
     buildpath = Path(build_dir_str)
     clean_buildpath(buildpath)
 
     site = Site.make_site()
     site.outpath = buildpath
-    site.contexts = [(".*.html", load_page_data)]
+
+    load_page_data_callable = partial(load_page_data, base_url=custom_domain_cli_arg)
+    site.contexts = [(".*.html", load_page_data_callable)]
     # disable automatic reloading
     site.render(use_reloader=False)
 
