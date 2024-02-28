ab34ed137888f434b37ca001cff31b4fb5a0ad90
Don Naro <dnaro@redhat.com>
2024-02-28 14:13:16 +0000

    add pip compile session
    Co-authored-by: Sviatoslav Sydorenko (Святослав Сидоренко) <wk.cvs.github@sydorenko.org.ua>

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,27 @@
 import nox
 
+@nox.session(name="pip-compile", python=["3.11"])
+def pip_compile(session: nox.Session):
+    # .pip-tools.toml was introduced in v7
+    session.install("pip-tools >= 7")
+
+    # Use --upgrade by default unless a user passes -P.
+    upgrade_related_cli_flags = "-P", "--upgrade-package", "--no-upgrade"
+    has_upgrade_related_cli_flags = any(
+        arg.startswith(upgrade_related_cli_flags)
+        for arg in session.posargs
+    )
+    injected_extra_cli_args = () if has_upgrade_related_cli_flags else ("--upgrade",)
+
+    session.run(
+        "pip-compile",
+        "--output-file",
+        f"requirements.txt",
+        *session.posargs,
+        *injected_extra_cli_args,
+        f"requirements.in",
+    )
+
 @nox.session(python=["3.11"]) # The python version should match the readthedocs configuration.
 def build(session: nox.Session):
     session.install(
